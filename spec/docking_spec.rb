c7c29848a4ea4201a8227301d607fc67f819a64d
Alex Jukes <alexjukes@Alexs-MacBook-Pro-2.local>
2017-03-16 11:00:56 +0000

    ex 10: bike is created and is working

--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -2,4 +2,9 @@ require 'docking_station.rb'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+
+  it 'gets a bike from docking station and expects it to be working' do
+    bike_56 = subject.release_bike
+    expect(bike_56).to be_working
+  end
 end
