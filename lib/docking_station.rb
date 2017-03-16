b119622b26c7032377484f074885eaa164a309ce
Alex Jukes <alexjukes@Alexs-MacBook-Pro-2.local>
2017-03-16 16:37:51 +0000

    Ex 13 passed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,17 @@
 class DockingStation
   def release_bike
-    if @bike.nil?
+    if @bike_rack.nil?
     raise "No bikes available."  # Guard condition
     else
     Bike.new
   end
   end
   def dock_bike(bike_56)
-    @bike = bike_56
+    if @bike_rack.nil?
+      @bike_rack = bike_56
+    else
+      raise "Docking station is full"
+    end
   end
-  attr_reader :bike
+  attr_reader :bike_rack
 end
