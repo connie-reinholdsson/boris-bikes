8a13b39a114b79bfef9b367da94968fefd33e092
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-03-17 11:18:52 +0000

    Used the Single Responsibility Principle to refactor our code and make methods full? and empt? private

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,18 +6,28 @@ def initialize
 end
 
   def release_bike
-    if @bike_rack.empty?
+    if empty?
     raise "No bikes available."  # Guard condition
     else
-    @bike_rack.pop
+    bike_rack.pop
   end
 
   end
   def dock_bike(bike_56)
-    if @bike_rack.count < 20
-      @bike_rack << bike_56
+    if full?
+    raise "Docking station is full"
     else
-      raise "Docking station is full"
-    end
+    bike_rack << bike_56
   end
+end
+
+private
+def full?
+  true if bike_rack.count >= 20
+end
+
+def empty?
+  true if bike_rack.empty?
+end
+
 end
